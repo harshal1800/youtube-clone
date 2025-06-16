ce3d473af6a88ef2858a5f7083c3d11c3b02f492
harshal1800 <harshalchavhank18@gmail.com>
2025-06-16 17:28:25 +0530

    Fix MongoDB authentication with correct credentials

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 app.get('/', (req, res) => {
   res.send('YouTube Clone Server is running!');
