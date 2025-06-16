a0f2880838aff5e92d369f8ffe709a52d6ac0775
harshal1800 <harshalchavhank18@gmail.com>
2025-06-17 00:47:14 +0530

    Update .gitignore in root directory and complete React front-end setup

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <h1 className="text-2xl font-bold text-center mt-10">
+          YouTube Clone
+        </h1>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
